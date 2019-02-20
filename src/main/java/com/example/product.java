31b4e48643b70df21face1352d83860f81f48203
aaa <aaa>
2019-02-20 21:07:32 +0800

    222

--- src/main/java/com/example/product.java
+++ src/main/java/com/example/product.java
@@ -3,5 +3,15 @@ package com.example;
 public class product {
     String sku;
 
+    public String getSku() {
+        return sku;
+    }
 
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public product(String sku) {
+        this.sku = sku;
+    }
 }
